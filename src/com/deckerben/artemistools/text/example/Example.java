c9bc7a4cb0ba1bf6945729a690a9a9b735234765
Crafterchen2 <Decker_ben@outlook.de>
2023-11-13 19:00:55 +0100

    Added an example

---
+++ src/com/deckerben/artemistools/text/example/Example.java
@@ -0,0 +1,33 @@
+package com.deckerben.artemistools.text.example;
+
+public class Example {
+
+    public static void main(String[] args) {
+        //Feel free to adjust these values
+        int totalApples = 10;
+        int threshold = 0;
+        int eatPerSession = 2;
+        //START demo code
+        if (totalApples > 0) {
+            System.out.println(ExampleTexts.INTRO);
+            System.out.println(ExampleTexts.AMOUNT.withArgs(totalApples));
+            do {
+                int nEaten = totalApples/eatPerSession;
+                totalApples = totalApples-nEaten;
+                if (nEaten <= threshold) {
+                    System.out.println(ExampleTexts.EAT_ALL.withArgs(threshold+1));
+                    totalApples = 0;
+                }   else {
+                    System.out.println(ExampleTexts.EAT.withArgs(nEaten,totalApples));
+                }
+            } while (totalApples > 0);
+            System.out.println(ExampleTexts.EMPTY);
+            System.out.println(ExampleTexts.OUTRO);
+        } else if (totalApples == 0) {
+            System.out.println(ExampleTexts.EMPTY);
+        } else {
+            System.out.println(ExampleTexts.ERROR);
+        }
+    }
+
+}
---
+++ src/com/deckerben/artemistools/text/example/ExampleTexts.java
@@ -0,0 +1,29 @@
+package com.deckerben.artemistools.text.example;
+
+import com.deckerben.artemistools.text.Argumentable;
+
+import static com.deckerben.artemistools.text.Argumentable.makeArg;
+
+public enum ExampleTexts implements Argumentable {
+
+    INTRO("Hello apple friends!"),
+    AMOUNT("We have "+makeArg(0)+" apples."),
+    EAT("We will have to eat "+makeArg(0)+" apples, to have "+makeArg(1)+" apples left."),
+    EAT_ALL("Now we will eat the remaining "+makeArg(0)+" apples."),
+    EMPTY("We are out of apples!"),
+    OUTRO("See you next time!"),
+    ERROR("We don't like negative apples!!"),
+    ;
+
+    private final String msg;
+
+    ExampleTexts(String msg){
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return msg;
+    }
+
+}
